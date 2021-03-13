54181a36763edb2c1db067663f0c2e78f9eae8ef
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-13 19:29:27 +0200

    создал

---
+++ lesson1/hw/js/script2.js
@@ -0,0 +1,21 @@
+var a = prompt("введите значинеи а:");
+console.log("значение для а = " + a);
+
+var b = prompt("Введите значение для b:");
+console.log("значение для b = " + b);
+
+var c;
+
+document.write("Переменная х равна = " + a + " , а переменная у =" + b + '<br>');
+
+c = a;
+console.log(" Значение для c = a: " + c);
+
+a = b;
+console.log(" Значение для a = b: " + a);
+
+b = c;
+console.log(" Значение для b = c: " + b);
+
+document.write("Переменная х равна = " + a + " , а переменная у =" + b + '<br>');
+
