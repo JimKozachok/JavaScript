638eaeeb6123bc67ad310d84b913a94740026ef1
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-14 16:59:49 +0200

    4 пока не сделал

--- lesson2/hw/scripts/script3.js
+++ lesson2/hw/scripts/script3.js
@@ -1,19 +1,18 @@
 //Определить какое из трех, введенных пользователем, чисел максимальное и вывести его на экран.
 
-
 var a=+prompt("Enter a");
+
 var b=+prompt("Enter b");
+
 var c=+prompt("Enter c");
 
 
 if (a>b && a>c) {
-    document.write('max a= ', a);
+    document.write('max a= '+ a);
     
 } else if (b>c && b>a ) {
-    document.write('max b= ', b);
+    document.write('max b= '+ b);
 
 } else if (c>a && c>b) {
-    document.write('max c= ', c );
-} 
-
-
+    document.write('max c= '+ c );
+}; 
