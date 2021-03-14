31341c107cf95bae073883a58cc6192a231dacad
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-14 16:58:45 +0200

    4 пока не сделал

--- lesson2/script3.js
+++
@@ -1,3 +0,0 @@
-var name = "Jhon";
-var admin = name;
-alert(admin);
--- lesson2/script5.js
+++
@@ -1,6 +0,0 @@
-  
-var question = prompt("Введите ваш возраст");
-
-var answer = confirm("Вам точно " +  question + "лет?");
-
-alert(answer);
