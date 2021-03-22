36bbb3e3cbd4d3bb44e549717ca358d2960ed8cc
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-22 14:12:21 +0200

    cw3 is done

--- lesson3/cw/scripts/script12.js
+++ lesson3/cw/scripts/script12.js
@@ -7,3 +7,12 @@
 
 var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
+var number = +prompt('число?');
+var count = 0;
+for (let i = 0; i < myArray.length; i++) {
+  if (myArray[i] === number) {
+    count++;
+  }
+}
+
+console.log(count);
