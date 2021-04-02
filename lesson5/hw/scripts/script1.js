df043b20d8b88c8a2960bb31d63d11ff84b25ff8
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-04-02 14:44:12 +0300

    hw 5 task 1 done

--- lesson5/hw/scripts/script1.js
+++ lesson5/hw/scripts/script1.js
@@ -1,1 +1,32 @@
-// 1. Создать объект Point, содержащий два свойства: "x" и "y" — координаты точки, и метод GetQuadrant, вычисляющий, в каком квадранте в декартовой системе координат находится данная точка. Протестируйте данный объект, изменяя значения его координат, и повторно вызавая метод GetQuadrant.
+// 1. Создать объект Point, содержащий два свойства: "x" и "y" — координаты точки, и метод GetQuadrant, вычисляющий, в каком квадранте в 
+// декартовой системе координат находится данная точка. Протестируйте данный объект, изменяя значения его координат, и повторно вызавая метод GetQuadrant.
+
+
+
+let a = +prompt("Enter the coordinates (x):");
+let b = +prompt("Enter the coordinates (y):");
+
+if (isNaN(a) || isNaN(b)) {
+    console.log("'x' or 'y' isn't a number!");
+}
+
+let point ={
+    x: a,
+    y: b
+};
+console.log(point);
+
+function GetQuadrant () {
+    if (point.x >0 && point.y >0) {
+        console.log("Point in the first quadrant");
+    } else if (point.x > 0 && point.y < 0) {
+        console.log("Point in the second quadrant");
+    } else if (point.x > 0 && point.y > 0) {
+        console.log("Point in the third quadrant");
+    }  else if (point.x < 0 && point.y >0 ) {
+        console.log("Point in the fourth quadrant");
+    } else if (point.x == 0 && point.y == 0 ) {
+        console.log("Zero point")
+    }
+}    
+GetQuadrant();
