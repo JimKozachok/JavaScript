8acb0d78806e3db9602e21099cfd53ee5fff64f2
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-04-10 10:46:04 +0300

    hw5 task 3 and 4 are done

--- lesson5/hw/scripts/script3.js
+++ lesson5/hw/scripts/script3.js
@@ -1,3 +1,57 @@
 // 3. Создать массив, который будет хранить в себе информацию о сотрудниках предприятия. Каждый элемент масива — объект,
 // содержащий свойства: name, sName, age, occupation, и метод show, который выводит всю информацию о пользователе.
 // Реализовать заполнение массива пользователем. По окончанию заполнения — вывести информацию о сотрудниках.
+
+var company = [
+
+    employe1 = {
+
+        name: a = prompt("Enter first name:"), 
+        sName: b = prompt("Enter surname:"),
+        age: c = prompt("Enter age:"),
+        occupation: d = prompt("Enter an occupation:"),
+
+        show: function() {
+           console.log("First name: " + this.name);
+           console.log("Surname: " + this.sName);
+           console.log("Age: " + this.age);
+           console.log("Occupation: " + this.occupation)
+        }
+    },
+              employe2 = {
+
+        name: a = prompt("Enter first name:"), 
+        sName: b = prompt("Enter surname:"),
+        age: c = prompt("Enter age:"),
+        occupation: d = prompt("Enter an occupation:"),
+
+        show: function() {
+           console.log("First name: " + this.name);
+           console.log("Surname: " + this.sName);
+           console.log("Age: " + this.age);
+           console.log("Occupation: " + this.occupation)
+        }
+    },
+              employe3 = {
+
+        name: a = prompt("Enter first name:"), 
+        sName: b = prompt("Enter surname:"),
+        age: c = prompt("Enter age:"),
+        occupation: d = prompt("Enter an occupation:"),
+
+        show: function() {
+           console.log("First name: " + this.name);
+           console.log("Surname: " + this.sName);
+           console.log("Age: " + this.age);
+           console.log("Occupation: " + this.occupation)
+        }
+    },
+
+]
+
+for (var i = 0; i < company.length; i++) {
+    company[i].show();
+}
+  
+
+
--- lesson5/hw/scripts/script4.js
+++ lesson5/hw/scripts/script4.js
@@ -10,3 +10,50 @@
 // // sort(function(a, b) { return a > b });
 // // 1, 2, 3, 5, 10, 14
 // // 1, 10, 14, 2, 3,
+var company = [
+employe1 = {
+    name: a = prompt("Enter first name:"), 
+    sName: b = prompt("Enter surname:"),
+    age: c = prompt("Enter age:"),
+    occupation: d = prompt("Enter an occupation:"),
+},
+employe2 = {
+    name: a = prompt("Enter first name:"), 
+    sName: b = prompt("Enter surname:"),
+    age: c = prompt("Enter age:"),
+    occupation: d = prompt("Enter an occupation:"),
+},
+employe3 = {
+    name: a = prompt("Enter first name:"), 
+    sName: b = prompt("Enter surname:"),
+    age: c = prompt("Enter age:"),
+    occupation: d = prompt("Enter an occupation:"),
+}
+]
+
+function addSalary(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].occupation == "director") {
+            arr[i].salary = 3000;            
+        }
+        else if (arr[i].occupation == "manager") {
+            arr[i].salary = 1500;
+        }
+        else if (arr[i].occupation == "programmer") {
+            arr[i].salary = 2000;
+        }
+        else {
+            arr[i].salary = 1000;
+        }
+    }
+}
+
+addSalary(company);
+
+for (var i = 0; i < company.length; i++) {
+console.log("First name: " + company[i].name);
+console.log("Surname: " + company[i].sName);
+console.log("Age: " + company[i].age + " years old");
+console.log("Occupation: " + company[i].occupation);
+console.log("Salary: " + company[i].salary);
+}
